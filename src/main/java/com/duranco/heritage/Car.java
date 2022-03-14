26d0cc11a0b9f3bd287b392fd4a317f111750493
khalil <khalillakhdharatc@gmail.com>
2022-03-14 12:02:39 +0100

    heritage

--- src/main/java/com/duranco/heritage/Car.java
+++ src/main/java/com/duranco/heritage/Car.java
@@ -1,5 +1,22 @@
 package com.duranco.heritage;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Car extends Passenger {
+private int noofdoors;
+
+public int getNoofdoors() {
+	return noofdoors;
+}
+
+public void setNoofdoors(int noofdoors) {
+	this.noofdoors = noofdoors;
+}
+
+
+
+
+
 
 }
--- src/main/java/com/duranco/heritage/Truck.java
+++ src/main/java/com/duranco/heritage/Truck.java
@@ -1,5 +1,21 @@
 package com.duranco.heritage;
 
-public class Truck {
+import javax.persistence.Entity;
+
+@Entity
+public class Truck extends Transportation {
+	private int noofcontainers;
+
+	public int getNoofcontainers() {
+		return noofcontainers;
+	}
+
+	public void setNoofcontainers(int noofcontainers) {
+		this.noofcontainers = noofcontainers;
+	}
+	
+	
+	
+	
 
 }
