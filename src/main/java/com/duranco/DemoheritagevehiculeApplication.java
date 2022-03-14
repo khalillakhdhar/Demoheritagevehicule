c515eb2e603c1e88ca53925a04325ed4bdaff9a8
khalil <khalillakhdharatc@gmail.com>
2022-03-14 11:49:55 +0100

    Initial commit

---
+++ src/main/java/com/duranco/DemoheritagevehiculeApplication.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoheritagevehiculeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoheritagevehiculeApplication.class, args);
+	}
+
+}
